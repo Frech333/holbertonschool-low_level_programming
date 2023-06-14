d2010111ab204c8908d5a5a9444297eb5cf238cc
Frech333 <josefrech@gmail.com>
2023-06-14 11:22:20 -0700

    Step 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#include <studio.h>
-puts "Programming is like building a multilingual puzzle"
+#include <stdio.h>                                                                                                                 
+/* main - Entry point                                                                                                              
+ *                                                                                                                                 
+ * return: Always 0 (Success)                                                                                                      
+ */                                                                                                                                
+                                                                                                                                   
+int main() {                                                                                                                       
+            puts("\"Programming is like building a multilingual puzzle");                                                          
+                return 0;                                                                                                          
+}
