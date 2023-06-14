a9a02205367429d7eca288355ba141303e3b9a3f
Frech333 <josefrech@gmail.com>
2023-06-14 11:27:30 -0700

    Step 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,5 +6,5 @@
                                                                                                                                    
 int main() {                                                                                                                       
             puts("\"Programming is like building a multilingual puzzle");                                                          
- Return: Always 0.                                                                                                          
+	    return 0;                                                                                                          
 }
