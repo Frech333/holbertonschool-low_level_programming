e55381098bd54abce964c901796c61cd14c2bc55
Frech333 <josefrech@gmail.com>
2023-06-19 16:15:15 -0700

    Step 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,28 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n;
+	int n;
 
-/* Initialize random seed */
-srand(time(NULL));
-/* Generate a random number between -100 and 100 */
-n = rand() % 201 - 100;
-/* Print the number */
-printf("%d is ", n);
-/* Check if the number is positive, zero, or negative */
-if (n > 0)
-{
-printf("positive\n");
-}
-else if (n == 0)
-{
-printf("zero\n");
-}
-else
-{
-printf("negative\n");
-}
-return 0;
+	/* Initialize random seed */
+	srand(time(NULL));
+
+	/* Generate a random number between -100 and 100 */
+	n = rand() % 201 - 100;
+
+	/* Print the number */
+	printf("%d is ", n);
+
+	/* Check if the number is positive, zero, or negative */
+	if (n > 0)
+	{
+		printf("positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("zero\n");
+	}
+	else
+	{
+		printf("negative\n");
+	}
+
+	return (0);
 }
