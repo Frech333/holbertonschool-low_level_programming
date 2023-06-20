e8394bfaf2b5466f8889833a92eb1dbaefdd3567
Frech333 <josefrech@gmail.com>
2023-06-20 11:42:13 -0700

    Step 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
-/**                                                                                                    
- * main - Entry point of the program                                                                   
- *                                                                                                     
- * Return: Always 0                                                                                    
+/** 
+ * main - Entry point of the program
+ *                                                                   
+ * Return: Always 0
  */
 int main(void)
 {
