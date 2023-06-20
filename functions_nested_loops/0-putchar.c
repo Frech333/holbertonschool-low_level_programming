d632ff9383f6f54293594083b35b3b5ff253b7d2
Frech333 <josefrech@gmail.com>
2023-06-20 11:51:46 -0700

    Step 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-  
-  char c = '_';
-  write(1, &c, 1);
-  write(1, "p", 1);
-  write(1, "u", 1);
-  write(1, "t", 1);
-  write(1, "c", 1);
-  write(1, "h", 1);
-  write(1, "a", 1);
-  write(1, "r", 1);
-  write(1, "\n", 1);
-  return (0);
+    char c = '_';
+
+    write(1, &c, 1);
+    write(1, "p", 1);
+    write(1, "u", 1);
+    write(1, "t", 1);
+    write(1, "c", 1);
+    write(1, "h", 1);
+    write(1, "a", 1);
+    write(1, "r", 1);
+    write(1, "\n", 1);
+    return (0);
 }
