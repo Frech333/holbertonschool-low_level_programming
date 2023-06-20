f08716184965b3ffc6752971c39edc31b044e53a
Frech333 <josefrech@gmail.com>
2023-06-19 20:00:39 -0700

    Step 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h
-#include <time.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 
-int main (void)
-  
+int main(void)
 {
-  int d;
+    int d;
 
-  for (d = '0'; d<= '9'; d++)
-  {
-  putchar(d);
-  if (d != '9')
-  {
-    putchar(',');
-    putchar(' ');
-  }
-  }
-  putchar('\n');
-  return (0);
+    for (d = '0'; d <= '9'; d++)
+    {
+        putchar(d);
+        if (d != '9')
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+    return (0);
 }
