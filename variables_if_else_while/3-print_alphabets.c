3c817a502576ab023358b294a09eb7a8a21493fa
Frech333 <josefrech@gmail.com>
2023-06-19 18:12:23 -0700

    Step 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabet in lowercase, then uppercase, using putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	/* Print lowercase alphabet */
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+
+	/* Print uppercase alphabet */
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+		putchar(uppercase);
+
+	putchar('\n');
+
+	return (0);
+}
