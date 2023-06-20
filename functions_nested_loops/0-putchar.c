7b19b40c63728c034aaa8ed5b335c560b8eed271
Frech333 <josefrech@gmail.com>
2023-06-20 11:40:16 -0700

    Step 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include <unistd.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
+/**                                                                                                    
+ * main - Entry point of the program                                                                   
+ *                                                                                                     
+ * Return: Always 0                                                                                    
  */
 int main(void)
 {
-	char c = '_';
+        char c = '_';
 
-	write(1, &c, 1);
-	write(1, "p", 1);
-	write(1, "u", 1);
-	write(1, "t", 1);
-	write(1, "c", 1);
-	write(1, "h", 1);
-	write(1, "a", 1);
-	write(1, "r", 1);
-	write(1, '"\n"', 1);
+        write(1, &c, 1);
+        write(1, "p", 1);
+        write(1, "u", 1);
+        write(1, "t", 1);
+        write(1, "c", 1);
+        write(1, "h", 1);
+        write(1, "a", 1);
+        write(1, "r", 1);
+        write(1, "\n", 1);
 
-	return (0);
+        return (0);
 }
