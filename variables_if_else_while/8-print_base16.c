705b805e8a94e6cb6a2ae9a4782fbe8d8796959b
Frech333 <josefrech@gmail.com>
2023-06-19 19:38:32 -0700

    Step 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 16; i++)
+    {
+        if (i < 10)
+            putchar(i + '0');  // Print digits 0-9
+        else
+            putchar(i + 'a' - 10);  // Print lowercase letters a-f
+    }
+
+    putchar('\n');
+
+    return 0;
+}
