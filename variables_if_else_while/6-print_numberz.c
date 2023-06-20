644a896ff7d03c5aabc77008b23fa9591e43cd0e
Frech333 <josefrech@gmail.com>
2023-06-19 19:29:50 -0700

    Step 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all single-digit numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	int num;
