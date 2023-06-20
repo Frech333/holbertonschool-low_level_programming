bce64c5f5d0d1a4bc247503856764168cf89a890
Frech333 <josefrech@gmail.com>
2023-06-20 12:34:36 -0700

    Step 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet - Make the alphabet
+ * Return: void
  */
+
 void print_alphabet(void)
+
 {
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-
-	_putchar('\n');
+  char c;
+  for (c = 'a'; c <= 'z'; c++)
+    {
+      _putchar(c);
+    }
+  _putchar('\n');
 }
