836079d109d0e7f57841c57c09a924b777b598ee
Frech333 <josefrech@gmail.com>
2023-06-19 18:58:34 -0700

    Step 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,35 @@
-#include <unistd.h>
+#include <stdio.h>
 
-/**                                                                                                    
- * main - Entry point of the program                                                                   
- *                                                                                                     
- * Return: Always 0                                                                                    
- */
-int main(void)
-{
-        int num;
-        char digit;
+int main(void) {
+    putchar('0');
+    putchar('\n');
 
-        for (num = 0; num < 10; num++)
-        {
-                digit = '0' + num; // Convert num to character representation
-                write(1, &digit, 1);
-        }
-        write(1, "\n", 1);
+    putchar('1');
+    putchar('\n');
 
-        return (0);
+    putchar('2');
+    putchar('\n');
+
+    putchar('3');
+    putchar('\n');
+
+    putchar('4');
+    putchar('\n');
+
+    putchar('5');
+    putchar('\n');
+
+    putchar('6');
+    putchar('\n');
+
+    putchar('7');
+    putchar('\n');
+
+    putchar('8');
+    putchar('\n');
+
+    putchar('9');
+    putchar('\n');
+
+    return 0;
 }
