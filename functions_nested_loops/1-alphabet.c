3c66b5ee2821b5cd7382126b8d6bfcf3820e3792
Frech333 <josefrech@gmail.com>
2023-06-20 18:23:09 -0700

    Step 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make the alphabet
+ print_alphabet - Make the alphabet
  * Return: void
  */
 
 void print_alphabet(void)
-
 {
   char c;
   for (c = 'a'; c <= 'z'; c++)
-    {
-      putchar(c);
-    }
-  putchar('\n');
+  {
+  _putchar(c);
+  }
+  _putchar('\n');
 }
