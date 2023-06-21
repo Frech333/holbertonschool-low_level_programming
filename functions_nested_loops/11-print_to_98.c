21b34779f05f21ce350d262e7e8c5d4e7a69cc7a
Frech333 <josefrech@gmail.com>
2023-06-21 07:13:09 -0700

    Step 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,25 @@
+#include <stdio.h>
+
+void print_to_98(int n); // Function prototype
+
 int main()
 {
     int number = 10;
     print_to_98(number);
     return 0;
+}
+
+void print_to_98(int n)
+{
+    if (n <= 98) {
+        for (; n < 98; n++) {
+            printf("%d, ", n);
+        }
+    } else {
+        for (; n > 98; n--) {
+            printf("%d, ", n);
+        }
+    }
+
+    printf("98\n");
 }
