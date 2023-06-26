6eb7e340388a00d5a20929b090d8bd92902b5531
Frech333 <josefrech@gmail.com>
2023-06-26 16:58:34 -0700

    Step 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -8,9 +8,8 @@
  * Return: nothing
  */
 void swap_int(int *a, int *b)
-/* the function that swaps the values of two integers. */
 {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
