e9255c03ff0dd949fcefdc6189596b707f4979e4
Frech333 <josefrech@gmail.com>
2023-06-26 09:32:45 -0700

    Step 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,12 +1,13 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char &str);
-void print_rev(char *s);
-void rev_string(char *s);
-coid puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void _puts(char *str);
+void puts2(char *str);
+/* Add other function prototypes here */
+
+/* Prototype for _putchar */
+int _putchar(char c);
+
+#endif /* MAIN_H */
