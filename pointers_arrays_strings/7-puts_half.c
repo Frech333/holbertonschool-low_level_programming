f382f6c377d6636c387f125de8680519c52ca974
Frech333 <josefrech@gmail.com>
2023-06-27 16:17:37 -0700

    Step 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void puts_half(char *str);
+
+/**
+ * puts_half - Prints the second half of a string
+ * @str: The input string
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start;
+
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = (length - 1) / 2;
+
+	for (; start < length; start++)
+		putchar(str[start]);
+
+	putchar('\n');
+}
