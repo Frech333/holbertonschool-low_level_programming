a788a4ef022913a84d3d89bb84f3e11e62f65671
Frech333 <josefrech@gmail.com>
2023-06-27 16:30:18 -0700

    Step 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_array(int *a, int n);
+
+/**
+ * print_array - Prints n elements of an integer array
+ * @a: The input array
+ * @n: The number of elements to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++) {
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
