b345695ce462588b1969f6333c2c4cc29623b467
Frech333 <josefrech@gmail.com>
2023-06-29 15:33:43 -0700

    Step 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h> /* Include <stddef.h> for NULL definition */
 
 /**
  * _strstr - Locates a substring
@@ -13,16 +14,17 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 
 	if (*needle == '\0')
-		return (haystack);
+	  return (haystack);
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; haystack[i + j] == needle[j]; j++)
 		{
 			if (needle[j + 1] == '\0')
-				return (&haystack[i]);
+			  return (&haystack[i]);
 		}
 	}
 
+	/* Substring not found, return NULL */
 	return (NULL);
 }
