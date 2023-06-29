4f18a605e2cc9c317a217d387b40c33485890dc7
Frech333 <josefrech@gmail.com>
2023-06-28 17:17:33 -0700

    Step 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,17 +10,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
+        int dest_len, i;
 
-	/* Find the length of the destination string */
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-		;
+        /* Find the length of the destination string */
+        for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+                ;
 
-	/* Concatenate up to n bytes from src to dest */
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
+        /* Concatenate up to n bytes from src to dest */
+        for (i = 0; i < n && src[i] != '\0'; i++)
+                dest[dest_len + i] = src[i];
 
-	dest[dest_len + i] = '\0'; /* Add null terminator to the concatenated string */
+        dest[dest_len + i] = '\0'; /* Add null terminator to the concatenated string */
 
-	return dest;
+        return dest;
 }
