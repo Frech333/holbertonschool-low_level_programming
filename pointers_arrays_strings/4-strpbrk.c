f1b292b953f07685fb3bdcbcec0bf23a5ed64fe7
Frech333 <josefrech@gmail.com>
2023-06-29 15:18:23 -0700

    Step 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -7,21 +7,21 @@
  * @accept: The set of bytes to search for.
  *
  * Return: A pointer to the byte in @s that matches one of the bytes in @accept,
- * or NULL if no such byte is found.
+ *         or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
 	if (s == NULL || accept == NULL)
-	  return (NULL);
+		return (NULL);
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-			  return (&s[i]);
+				return (&s[i]);
 		}
 	}
 
