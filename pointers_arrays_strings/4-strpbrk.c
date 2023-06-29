cc0e53e9a0856f90daa697daf2266e56cedb889a
Frech333 <josefrech@gmail.com>
2023-06-29 15:20:03 -0700

    Step 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -6,7 +6,7 @@
  * @s: The string to search.
  * @accept: The set of bytes to search for.
  *
- * Return: A pointer to the byte in @s that matches one of the bytes in @accept,
+ * Return: A pointer to the byte in @s that matches one of the bytes in @accept
  *         or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
