2036cf08580d77d02cb93c54424a23c327b00465
Frech333 <josefrech@gmail.com>
2023-06-29 16:03:58 -0700

    Step 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
 void print_diagsums(int *a, int size)
 {
-  int i;
-  int sum_diag1 = 0;
-  int sum_diag2 = 0;
+  int i, n, sum1 = 0, sum2 = 0;
 
-  for (i = 0; i < size; i++)
-  {
-    sum_diag1 += *(a + i * size + i);      /* Add elements from the first diagonal */
-    sum_diag2 += *(a + i * size + size - 1 - i);  /* Add elements from the second diagonal */
-  }
+  for (i = 0; i <= (size * size); i = i + size + 1)
+    sum1 = sum1 + a[i];
 
-  printf("Sum of the first diagonal: %d\n", sum_diag1);
-  printf("Sum of the second diagonal: %d\n", sum_diag2);
+  for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+    sum2 = sum2 +a [n];
+  printf("%d, %d\n", sum1, sum2);
 }
