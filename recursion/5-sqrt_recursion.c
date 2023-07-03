100814aae4c46043338e7b4ec3548a25504bc8c4
Frech333 <josefrech@gmail.com>
2023-07-03 12:13:02 -0700

    Step 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -12,11 +12,11 @@
 int sqrt_helper(int n, int guess)
 {
 	if (guess * guess == n)
-	  return (guess);
+		return (guess);
 	else if (guess * guess > n)
-	  return (-1);
+		return (-1);
 	else
-	  return (sqrt_helper(n, guess + 1));
+		return (sqrt_helper(n, guess + 1));
 }
 
 /**
@@ -28,7 +28,7 @@ int sqrt_helper(int n, int guess)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	  return (-1);
+		return (-1);
 	else
-	  return (sqrt_helper(n, 0));
+		return (sqrt_helper(n, 0));
 }
