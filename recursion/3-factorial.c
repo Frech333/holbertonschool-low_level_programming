67872463eb2bf4aa3211796a7ef2cee9740beda0
Frech333 <josefrech@gmail.com>
2023-07-03 11:44:24 -0700

    step 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,19 +9,11 @@
  */
 int factorial(int n)
 {
-  int result = 1;
-  int i;
-
   if (n < 0)
     return -1;
 
   if (n == 0)
     return 1;
 
-  for (i = 1; i <= n; i++)
-  {
-    result *= i;
-  }
-
-  return result;
+  return n * factorial(n - 1);
 }
