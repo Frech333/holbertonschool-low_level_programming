aabda669673f57003b0168b3267d9555c9b066ef
Frech333 <josefrech@gmail.com>
2023-07-03 12:28:21 -0700

    Step 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -11,10 +11,10 @@
 int is_prime_helper(int n, int divisor)
 {
 	if (divisor <= 1)
-	  return (1);
+		return (1);
 
 	if (n % divisor == 0)
-	  return (0);
+		return (0);
 
 	return (is_prime_helper(n, divisor - 1));
 }
@@ -28,7 +28,7 @@ int is_prime_helper(int n, int divisor)
 int is_prime_number(int n)
 {
 	if (n <= 1)
-	  return (0);
+		return (0);
 
 	return (is_prime_helper(n, n - 1));
 }
