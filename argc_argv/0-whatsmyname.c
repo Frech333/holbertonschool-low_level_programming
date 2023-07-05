e09c39a8e55ce6a260bbbc75c726cef8222321d4
Frech333 <josefrech@gmail.com>
2023-07-05 11:53:29 -0700

    Step 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,18 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - Prints its name
+ * @@argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
+int main(int argc, char*argv[])
 {
-	int i;
+  (void) argc;
+  printf("%s\n", argv[0]);
 
-	for (i = 0; argv[0][i] != '\0'; i++)
-		_putchar(argv[0][i]);
-
-	_putchar('\n');
-
-	return (0);
-}
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+  return (0);
 }
