98b541e870828f523d1873839e41325e711f6e33
Frech333 <josefrech@gmail.com>
2023-07-10 10:51:25 -0700

    Step 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -4,25 +4,25 @@
 
 char *_strdup(char *str)
 {
-	char *duplicate;
-	int length = 0;
-	int i;
+char *duplicate;
+int length = 0;
+int i;
 
-	if (str == NULL)
-	  return (NULL);
+if (str == NULL)
+return (NULL);
 
-	/* Calculate the length of the string */
-	while (str[length])
-		length++;
+/* Calculate the length of the string */
+while (str[length])
+length++;
 
-	/* Allocate memory for the duplicate string */
-	duplicate = malloc((length + 1) * sizeof(char));
-	if (duplicate == NULL)
-	  return (NULL);
+/* Allocate memory for the duplicate string */
+duplicate = malloc((length + 1) * sizeof(char));
+if (duplicate == NULL)
+return (NULL);
 
-	/* Copy the string */
-	for (i = 0; i <= length; i++)
-		duplicate[i] = str[i];
+/* Copy the string */
+for (i = 0; i <= length; i++)
+duplicate[i] = str[i];
 
-	return (duplicate);
+return (duplicate);
 }
