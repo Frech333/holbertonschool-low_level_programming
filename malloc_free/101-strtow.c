1f13158d1140d796a18039b7b4af2aa5ff436229
Frech333 <josefrech@gmail.com>
2023-07-10 11:43:00 -0700

    Step 6

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -4,11 +4,12 @@
  * argstostr - main entry
  * @ac: int input
  * @av: double pointer array
- * Return: 0
+ * Return: (0)
  */
 char *argstostr(int ac, char **av)
 {
 	int i, n, r = 0, l = 0;
+
 	char *str;
 
 	if (ac == 0 || av == NULL)
