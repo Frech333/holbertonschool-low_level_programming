16d45ca0be38f61c39af90854985895945c7005d
Frech333 <josefrech@gmail.com>
2023-07-13 08:57:02 -0700

    Step 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -7,7 +7,7 @@
  * @b: The size to allocate
  *
  * Return: Pointer to the allocated memory
- *         If malloc fails, the function terminates the program with exit status 98.
+ * If malloc fails, the function terminates the program with exit status 98.
  */
 void *malloc_checked(unsigned int b)
 {
