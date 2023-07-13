abd457411234b30dc1e3f97f5e8aba4b0f3037c3
Frech333 <josefrech@gmail.com>
2023-07-13 08:37:39 -0700

    Step 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
 
 /**
@@ -9,7 +8,6 @@
  *
  * Return: Nothing.
  */
-
 void *malloc_checked(unsigned int b)
 {
 void *p;
