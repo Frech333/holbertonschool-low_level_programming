49b8425256954c17fb2cec4950e06570b18289e3
Frech333 <josefrech@gmail.com>
2023-08-02 16:18:44 -0700

    Step 4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -4,11 +4,13 @@
  * free_dlistint - Frees a doubly linked list
  * @head: Pointer to the head of the list
  */
-void free_dlistint(dlistint_t *head) {
+void free_dlistint(dlistint_t *head)
+{
 dlistint_t *current, *next;
 
 current = head;
-while (current != NULL) {
+while (current != NULL)
+{
 next = current->next; /* Save the next node before freeing the current one */
 free(current); /* Free the current node */
 current = next; /* Move to the next node */
