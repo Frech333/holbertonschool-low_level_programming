319cecdf858ce026f0b6c56b9223db6fea1d8765
Frech333 <josefrech@gmail.com>
2023-08-08 15:20:49 -0700

    Step 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,39 +9,39 @@
  * Return: The actual number of letters read and printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters) {
-    int file_descriptor;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+int file_descriptor;
+ssize_t bytes_read, bytes_written;
+char *buffer;
 
-    if (filename == NULL)
-        return (0);
+if (filename == NULL)
+return (0);
 
-    buffer = malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL)
-        return (0);
+buffer = malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
+return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1) {
-        free(buffer);
-        return (0);
-    }
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor == -1) {
+free(buffer);
+return (0);
+}
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1) {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+bytes_read = read(file_descriptor, buffer, letters);
+if (bytes_read == -1) {
+free(buffer);
+close(file_descriptor);
+return (0);
+}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read) {
+free(buffer);
+close(file_descriptor);
+return (0);
+}
 
-    free(buffer);
-    close(file_descriptor);
+free(buffer);
+close(file_descriptor);
 
-    return (bytes_written);
+return (bytes_written);
 }
